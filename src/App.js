6864c4d50e76efe5153f3dd54c293a112f554f66
stangkaaipean <sanhanut440@gmail.com>
2020-01-04 18:43:25 +0700

    - finished an thinking-in-react tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,159 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class FilterableProductTable extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    }
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockOnly = this.handleInStockOnly.bind(this);
+  }
+
+  handleFilterTextChange(value) {
+    this.setState({ filterText: value });
+  }
+
+  handleInStockOnly(value) {
+    this.setState({ inStockOnly: value });
+  }
+
+  render() {
+    return (
+      <div className="filterableProductTable border-box clearfix">
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          handleFilterTextChange={this.handleFilterTextChange}
+          handleInStockOnly={this.handleInStockOnly}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+      </div >
+    )
+  }
+}
+
+class SearchBar extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+    this.handleInStockOnly = this.handleInStockOnly.bind(this);
+  }
+
+  handleFilterTextChange(e) {
+    this.props.handleFilterTextChange(e.target.value);
+  }
+
+  handleInStockOnly(e) {
+    this.props.handleInStockOnly(e.target.checked);
+  }
+
+  render() {
+    return (
+      <form className="inside-box searchBar border-box clearfix">
+        <input
+          type="text"
+          className="searchInput"
+          placeholder="Search..."
+          value={this.props.filterText}
+          onChange={this.handleFilterTextChange}
+        />
+        <p>
+          <input
+            type="checkbox"
+            checked={this.props.inStockOnly}
+            onChange={this.handleInStockOnly}
+          />
+          <label>Only show products in stock</label>
+        </p>
+      </form>
+    )
+  }
+}
+
+function ProductTable(props) {
+  const rows = [];
+  let lastCategroy;
+  const filterText = props.filterText;
+  const inStockOnly = props.inStockOnly;
+
+  props.products.forEach(product => {
+    if (product.name.indexOf(filterText) === -1) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+
+    if (lastCategroy !== product.category) {
+      lastCategroy = product.category;
+      rows.push(
+        <ProductCategoryRow
+          product={product}
+          key={product.category} />
+      )
+    }
+
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name} />
+    )
+  });
+
+  return (
+    <div className="inside-box productTable border-box clearfix">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    </div>
+  )
+}
+
+function ProductRow(props) {
+  const product = props.product;
+  const name = product.stocked ? product.name :
+    <span style={{ color: 'red' }}>
+      {product.name}
+    </span>
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  )
+}
+
+function ProductCategoryRow(props) {
+  return (
+    <tr>
+      <th colSpan="2">{props.product.category}</th>
+    </tr>
+  )
+}
+
+
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <FilterableProductTable products={props.products} />
+    </div >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const PRODUCTS = [
+  { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+  { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+  { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+  { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+  { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+];
+
+ReactDOM.render(<App products={PRODUCTS}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
